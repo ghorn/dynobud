c593027a13a942ab4e662068911ebf89336ecd3d
Greg Horn <gregmainland@gmail.com>
2013-11-04 10:22:26 +0100

    make IOSchemes helper module more polymorphic

--- IOSchemes.hs
+++ IOSchemes.hs
@@ -1,18 +1,22 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# Language ScopedTypeVariables #-}
 
-module IOSchemes where
+module IOSchemes ( mkSchemeSXMatrix
+                 , mkSchemeCRSSparsity
+                 ) where
 
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Foldable as F
 import qualified Data.Vector as V
 
-import Casadi.Wrappers.Classes.SXMatrix
-import Casadi.Wrappers.Classes.IOScheme
+import Casadi.Wrappers.Classes.CRSSparsity ( CRSSparsity, crsSparsity' )
+import Casadi.Wrappers.Classes.SXMatrix ( SXMatrix, sxMatrix )
+import Casadi.Wrappers.Classes.IOScheme ( ioScheme', ioScheme_size, ioScheme_entry )
 import Casadi.Wrappers.Enums ( InputOutputScheme(..) )
 
-mkSchemeSXMatrix :: InputOutputScheme -> [(String,SXMatrix)] -> IO (V.Vector SXMatrix)
-mkSchemeSXMatrix schemeEnum userVals = do
+mkScheme :: forall a . IO a -> InputOutputScheme -> [(String,a)] -> IO (V.Vector a)
+mkScheme newEmpty schemeEnum userVals = do
   sch <- ioScheme' schemeEnum
   size <- ioScheme_size sch
   let ks = take size [(0::Int)..]
@@ -20,8 +24,8 @@ mkSchemeSXMatrix schemeEnum userVals = do
   let entrySet :: S.Set String
       entrySet = S.fromList entries
 
-      -- map of user input {String:SXMatrix}, already checked for duplicates
-      userMap :: M.Map String SXMatrix
+      -- map of user input {String:a}, already checked for duplicates
+      userMap :: M.Map String a
       userMap = M.fromListWithKey err userVals
       err k _ _ = error $ "mkScheme: " ++ show schemeEnum ++ ": you gave key \"" ++ k ++ "\" more than once"
 
@@ -32,10 +36,10 @@ mkSchemeSXMatrix schemeEnum userVals = do
     [] -> return ()
     xs ->  error $ "mkScheme: " ++ show schemeEnum ++ ": you gave keys " ++ show xs ++ " which aren't valid, valid entries:\n" ++ show entries
 
-  let getSXMat str = case M.lookup str userMap of
-        Nothing -> sxMatrix -- new empty sxmatrix
+  let getElem str = case M.lookup str userMap of
+        Nothing -> newEmpty -- new empty
         Just x -> return x
-  sxMatVec <- fmap V.fromList $ mapM getSXMat entries
+  vec <- fmap V.fromList $ mapM getElem entries
 
 --  name <- ioScheme_name sch
 --  print size
@@ -45,4 +49,10 @@ mkSchemeSXMatrix schemeEnum userVals = do
 --  mapM (ioScheme_entryEnum sch) ks >>= print
 --  mapM (ioScheme_entryLabel sch) ks >>= print
 
-  return sxMatVec
+  return vec
+
+mkSchemeSXMatrix :: InputOutputScheme -> [(String,SXMatrix)] -> IO (V.Vector SXMatrix)
+mkSchemeSXMatrix = mkScheme sxMatrix
+
+mkSchemeCRSSparsity :: InputOutputScheme -> [(String,CRSSparsity)] -> IO (V.Vector CRSSparsity)
+mkSchemeCRSSparsity = mkScheme crsSparsity'
