9a71b6a08a7d311c61cfce846e6834a79159f626
Greg Horn <gregmainland@gmail.com>
2013-12-11 13:17:30 +0100

    ipopt now has monadic interface using Vector, solveNlpIpopt is now on top of this

--- src/Hascm/Ipopt.hs
+++ src/Hascm/Ipopt.hs
@@ -1,10 +1,26 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# Language RankNTypes #-}
+{-# Language GeneralizedNewtypeDeriving #-}
 
-module Hascm.Ipopt ( solveNlpIpopt ) where
+module Hascm.Ipopt ( solveNlpIpopt
+                   , solve
+                   , setX0
+                   , setP
+                   , setLbx
+                   , setUbx
+                   , setLbg
+                   , setUbg
+                   , getX0
+                   , getP
+                   , getLbx
+                   , getUbx
+                   , getLbg
+                   , getUbg
+                   ) where
 
-import qualified Data.Vector as V
+import Control.Monad ( when )
+import Control.Monad.Reader ( MonadIO(..), MonadReader(..), ReaderT(..) )
 import Data.Maybe ( fromMaybe )
+import qualified Data.Vector as V
 
 import Casadi.Wrappers.Enums ( InputOutputScheme(..) )
 import Casadi.Callback
@@ -33,15 +49,112 @@ toBnds vs = (V.map (fromMaybe (-inf)) lb, V.map (fromMaybe inf) ub)
   where
     (lb,ub) = V.unzip vs
 
-solveNlpIpopt ::
-  forall x p g . (Vectorize x, Vectorize p, Vectorize g) =>
-  Nlp x p g -> x Double -> p Double -> Maybe (x Double -> IO Bool)
-  -> IO (Either String (NlpOut x g Double))
-solveNlpIpopt nlp x0 p0 callback' = do
-  (NlpInputs inputsX' inputsP', NlpFun obj g') <- funToSX (nlpFG nlp)
-  let inputsX = vectorize inputsX' :: V.Vector SX
-      inputsP = vectorize inputsP' :: V.Vector SX
-      g = vectorize g' :: V.Vector SX
+setInput :: (IpoptState -> Int) -> String -> V.Vector Double -> Ipopt ()
+setInput getLen name x = do
+  ipoptState <- ask
+  let nx' = V.length x
+      nx = getLen ipoptState
+  when (nx /= nx') $ error $ name ++ " dimension mismatch, " ++ show nx ++ " (true) /= " ++ show nx' ++ " (given)"
+  liftIO $ ioInterfaceFX_setInput''''' (isSolver ipoptState) x name
+  return ()
+
+getInput :: String -> Ipopt (V.Vector Double)
+getInput name = do
+  ipoptState <- ask
+  liftIO $ ioInterfaceFX_input'' (isSolver ipoptState) name >>= dmatrix_data
+
+setX0 :: V.Vector Double -> Ipopt ()
+setX0 = setInput isNx "x0"
+
+setLbx :: V.Vector Double -> Ipopt ()
+setLbx = setInput isNx "lbx"
+
+setUbx :: V.Vector Double -> Ipopt ()
+setUbx = setInput isNx "ubx"
+
+setLbg :: V.Vector Double -> Ipopt ()
+setLbg = setInput isNg "lbg"
+
+setUbg :: V.Vector Double -> Ipopt ()
+setUbg = setInput isNg "ubg"
+
+setP :: V.Vector Double -> Ipopt ()
+setP = setInput isNp "p"
+
+solve :: Ipopt (Either String (NlpOut V.Vector V.Vector Double))
+solve = do
+  ipoptState <- ask
+  let ipopt = isSolver ipoptState
+  liftIO $ do
+    fxSolveSafe ipopt
+
+    solveStatus <- fx_getStat ipopt "return_status"  >>= printableObject_getDescription
+
+    fopt <- fmap V.head $ ioInterfaceFX_output'' ipopt "f" >>= dmatrix_data
+    xopt <- ioInterfaceFX_output'' ipopt "x" >>= dmatrix_data
+    gopt <- ioInterfaceFX_output'' ipopt "g" >>= dmatrix_data
+    lamXOpt <- ioInterfaceFX_output'' ipopt "lam_x" >>= dmatrix_data
+    lamGOpt <- ioInterfaceFX_output'' ipopt "lam_g" >>= dmatrix_data
+    let lambdaOut = Multipliers { lambdaX = lamXOpt
+                                , lambdaG = lamGOpt
+                                }
+        nlpOut = NlpOut { fOpt = fopt
+                        , xOpt = xopt
+                        , gOpt = gopt
+                        , lambdaOpt = lambdaOut
+                        }
+    if solveStatus `elem` ["Solve_Succeeded", "Solved_To_Acceptable_Level"]
+      then return (Right nlpOut)
+      else return (Left solveStatus)
+
+
+getX0 :: Ipopt (V.Vector Double)
+getX0 = getInput "x0"
+
+getLbx :: Ipopt (V.Vector Double)
+getLbx = getInput "lbx"
+
+getUbx :: Ipopt (V.Vector Double)
+getUbx = getInput "ubx"
+
+getLbg :: Ipopt (V.Vector Double)
+getLbg = getInput "lbg"
+
+getUbg :: Ipopt (V.Vector Double)
+getUbg = getInput "ubg"
+
+getP :: Ipopt (V.Vector Double)
+getP = getInput "p"
+
+data IpoptState = IpoptState { isNx :: Int
+                             , isNg :: Int
+                             , isNp :: Int
+                             , isSolver :: IpoptSolver
+                             }
+newtype Ipopt a = Ipopt (ReaderT IpoptState IO a)
+                deriving ( Monad
+                         , MonadReader IpoptState
+                         , MonadIO
+                         )
+
+runIpopt ::
+  NlpInputs V.Vector V.Vector SX -> NlpFun V.Vector SX
+  -> Maybe (V.Vector Double -> IO Bool)
+  -> Ipopt a -> IO a
+runIpopt nlpInputs@(NlpInputs x p) nlpOutputs@(NlpFun _ g) callback (Ipopt ipoptMonad) = do
+  ipopt <- makeIpoptSolver nlpInputs nlpOutputs callback
+  let conf = IpoptState { isNx = V.length x
+                        , isNg = V.length g
+                        , isNp = V.length p
+                        , isSolver = ipopt
+                        }
+  liftIO $ runReaderT ipoptMonad conf
+
+makeIpoptSolver ::
+  NlpInputs V.Vector V.Vector SX -> NlpFun V.Vector SX
+  -> Maybe (V.Vector Double -> IO Bool)
+  -> IO IpoptSolver
+makeIpoptSolver (NlpInputs inputsX inputsP) (NlpFun obj g) callback' = do
   inputsXMat <- sxMatrix''''''''''' inputsX
   inputsPMat <- sxMatrix''''''''''' inputsP
   objMat    <- sxMatrix''''''''''' (V.singleton obj)
@@ -58,39 +171,52 @@ solveNlpIpopt nlp x0 p0 callback' = do
     Just callback -> do
       let cb fx' = do
             xval <- ioInterfaceFX_output fx' 0 >>= dmatrix_data
-            callbackRet <- callback (devectorize xval)
+            callbackRet <- callback xval
             return $ if callbackRet then 0 else 1
       casadiCallback <- makeCallback cb >>= genericTypeCallback
       optionsFunctionality_setOption ipopt "iteration_callback" casadiCallback
 
   sharedObject_init ipopt
 
+  return ipopt
+
+
+solveNlpIpopt ::
+  (Vectorize x, Vectorize p, Vectorize g) =>
+  Nlp x p g -> x Double -> p Double -> Maybe (x Double -> IO Bool)
+  -> IO (Either String (NlpOut x g Double))
+solveNlpIpopt nlp x0 p0 callback' = do
+  (NlpInputs inputsX' inputsP', NlpFun obj g') <- funToSX (nlpFG nlp)
+  let inputsX = vectorize inputsX' :: V.Vector SX
+      inputsP = vectorize inputsP' :: V.Vector SX
+      g = vectorize g' :: V.Vector SX
+      callback = fmap (. devectorize) callback'
+      nlpInputs = NlpInputs inputsX inputsP
+      nlpOutputs = NlpFun obj g
+
   let (lbx,ubx) = toBnds (vectorize $ nlpBX nlp)
       (lbg,ubg) = toBnds (vectorize $ nlpBG nlp)
-  ioInterfaceFX_setInput''''' ipopt (vectorize x0) "x0"
-  ioInterfaceFX_setInput''''' ipopt (vectorize p0) "p"
-  ioInterfaceFX_setInput''''' ipopt lbx "lbx"
-  ioInterfaceFX_setInput''''' ipopt ubx "ubx"
-  ioInterfaceFX_setInput''''' ipopt lbg "lbg"
-  ioInterfaceFX_setInput''''' ipopt ubg "ubg"
+      runMe = do
+        setX0 (vectorize x0)
+        setP (vectorize p0)
+        setLbx lbx
+        setUbx ubx
+        setLbg lbg
+        setUbg ubg
+        solve
 
-  fxSolveSafe ipopt
-
-  solveStatus <- fx_getStat ipopt "return_status"  >>= printableObject_getDescription
-
-  fopt <- fmap V.head $ ioInterfaceFX_output'' ipopt "f" >>= dmatrix_data
-  xopt <- ioInterfaceFX_output'' ipopt "x" >>= dmatrix_data
-  gopt <- ioInterfaceFX_output'' ipopt "g" >>= dmatrix_data
-  lamXOpt <- ioInterfaceFX_output'' ipopt "lam_x" >>= dmatrix_data
-  lamGOpt <- ioInterfaceFX_output'' ipopt "lam_g" >>= dmatrix_data
-  let lambdaOut = Multipliers { lambdaX = devectorize lamXOpt
-                              , lambdaG = devectorize lamGOpt
-                              }
-      nlpOut = NlpOut { fOpt = fopt
-                      , xOpt = devectorize xopt
-                      , gOpt = devectorize gopt
-                      , lambdaOpt = lambdaOut
-                      }
-  if solveStatus `elem` ["Solve_Succeeded", "Solved_To_Acceptable_Level"]
-    then return (Right nlpOut)
-    else return (Left solveStatus)
+  woo <- runIpopt nlpInputs nlpOutputs callback runMe
+  return $ case woo of
+    Left x -> Left x
+    Right (NlpOut { fOpt = fopt
+                  , xOpt = xopt
+                  , gOpt = gopt
+                  , lambdaOpt = Multipliers { lambdaX = lambdax
+                                            , lambdaG = lambdag
+                                            }}) ->
+      Right $ NlpOut { fOpt = fopt
+                     , xOpt = devectorize xopt
+                     , gOpt = devectorize gopt
+                     , lambdaOpt = Multipliers { lambdaX = devectorize lambdax
+                                               , lambdaG = devectorize lambdag
+                                               }}
