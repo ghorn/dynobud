7611b5ab5616a82e5749bf97f8aafd6c37860702
Greg Horn <gregmainland@gmail.com>
2014-01-20 14:06:57 +0100

    revive basic multiple shooting

--- src/Hascm/MultipleShooting.hs
+++ src/Hascm/MultipleShooting.hs
@@ -1,96 +1,121 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# Language RankNTypes #-}
-{-# Language FlexibleContexts #-}
+{-# Language ScopedTypeVariables #-}
 {-# Language DeriveFunctor #-}
 {-# Language DeriveGeneric #-}
-{-# Language GADTs #-}
+{-# Language RankNTypes #-}
 
 module Hascm.MultipleShooting
        ( MsTraj(..)
        , MsPoint(..)
        , MsConstraints(..)
+       , MsOcp(..)
        , makeNlp
        ) where
 
-import TypeVecs ( Vec, (|>), tvzipWith, tvhead, tvtail, tvlength )
-import Vectorize
-import Nlp
-import TypeNats
-import Ocp
-import Dae
+import qualified Data.Vector as V
+import Linear.Metric ( dot )
+import Linear.V ( Dim )
+
+import Hascm.TypeVecs ( Vec )
+import qualified Hascm.TypeVecs as TV
+import Hascm.Vectorize
+import Hascm.Nlp
+
+data MsOcp x u c h =
+  MsOcp { msMayer :: forall a. Floating a => x a -> V.Vector a
+        , msLagrange :: forall a. Floating a => x a -> u a -> V.Vector a
+        , msDae :: forall a. Floating a => x a -> u a -> x a
+        , msBc :: forall a. Floating a => x a -> x a -> c a
+        , msPathC :: forall a. Floating a => x a -> u a -> h a
+        , msPathCBnds :: h (Maybe Double, Maybe Double)
+        , msXbnd :: x (Maybe Double, Maybe Double)
+        , msUbnd :: u (Maybe Double, Maybe Double)
+        , msT :: Double
+        }
 
 -- multiple shooting
 data MsPoint x u a = MsPoint (x a) (u a) deriving (Functor, Generic1)
 instance (Vectorize x, Vectorize u) => Vectorize (MsPoint x u)
-data MsTraj x u p n a = MsTraj (p a) (Vec n (MsPoint x u a)) (x a) a deriving (Functor, Generic1)
-instance (Vectorize x, Vectorize u, Vectorize p, NaturalT n) => Vectorize (MsTraj x u p n)
+data MsTraj x u n a = MsTraj (Vec n (MsPoint x u a)) (x a) deriving (Functor, Generic1)
+instance (Vectorize x, Vectorize u, Dim n) => Vectorize (MsTraj x u n)
 
-data MsConstraints x n bc pc a =
+data MsConstraints x n c h a =
   MsConstraints
-  { mscBc :: bc a
-  , mscPathc :: Vec n (pc a)
+  { mscBc :: c a
+  , mscPathc :: Vec n (h a)
   , mscDynamics :: Vec n (x a)
   } deriving (Functor, Generic1, Show)
 
-instance (Vectorize x, Vectorize pc, Vectorize bc, NaturalT nu) =>
-         Vectorize (MsConstraints x nu bc pc)
+instance (Vectorize x, Vectorize c, Vectorize h, Dim n) =>
+         Vectorize (MsConstraints x n c h)
 
-getDvBnds :: forall x u p bc pc n . NaturalT n =>
-  OcpPhase x None u p x bc pc Double -> -- Double here supresses warning in makeNlp, Double is never used
-  MsTraj x u p n (Maybe Double, Maybe Double)
-getDvBnds ocp = MsTraj p xus x (Just lbt, Just ubt)
+getDvBnds ::
+  forall x u c h n . (Dim n) =>
+  MsOcp x u c h -> MsTraj x u n (Maybe Double, Maybe Double)
+getDvBnds ocp = MsTraj xus x
   where
-    xus = fill (MsPoint (ocpXbnd ocp) (ocpUbnd ocp))
-    x = ocpXbnd ocp
-    p = ocpPbnd ocp
-    (lbt, ubt) = ocpTbnd ocp
+    xus = fill (MsPoint (msXbnd ocp) (msUbnd ocp))
+    x = msXbnd ocp
 
-getGBnds :: (Vectorize x, Vectorize bc, NaturalT n) =>
-            OcpPhase x None u p x bc pc Double ->  -- Double here supresses warning in makeNlp, Double is never used
-            MsConstraints x n bc pc (Maybe Double, Maybe Double)
+getGBnds ::
+  (Vectorize x, Vectorize c, Dim n) =>
+  MsOcp x u c h ->
+  MsConstraints x n c h (Maybe Double, Maybe Double)
 getGBnds ocp =
   MsConstraints
   { mscBc = fill (Just 0, Just 0)
-  , mscPathc = fill (ocpPathCBnds ocp)
+  , mscPathc = fill (msPathCBnds ocp)
   , mscDynamics = fill (fill (Just 0, Just 0))
   }
 
-getFg :: (Floating a, Vectorize x, NaturalT n, PositiveT n, (n ~ Succ (Pred n))) =>
-         OcpPhase x None u p x bc pc a ->
-         Integrator x None u p x a ->
-         MsTraj x u p n a ->
-         NlpFun (MsConstraints x n bc pc) a
-getFg ocp integrator (MsTraj p xus xf tf) = NlpFun objective constraints
+getFg :: forall x u c h n a .
+  (Floating a, Vectorize x, Dim n) =>
+  MsOcp x u c h ->
+  (x a -> u a -> x a) ->
+  NlpInputs (MsTraj x u n) None a ->
+  NlpFun (MsConstraints x n c h) a
+getFg ocp integrator (NlpInputs (MsTraj xus xf) _) = NlpFun objective constraints
   where
     xs = fmap (\(MsPoint x _) -> x) xus
     us = fmap (\(MsPoint _ u) -> u) xus
-    n = tvlength us
-    ts = tf / (fromIntegral n)
+--    n = TV.tvlength us
+--    tf = msT ocp
+--    ts = tf / (fromIntegral n)
     constraints =
       MsConstraints
-      { mscBc = (ocpBc ocp) (tvhead xs) xf
-      , mscPathc = tvzipWith (\x u -> ocpPathC ocp x None u p) xs us
+      { mscBc = (msBc ocp) (TV.tvhead xs) xf
+      , mscPathc = TV.tvzipWith (\x u -> msPathC ocp x u) xs us
       , mscDynamics = vzipWith3 integrator' xs us x1s
       }
       where
         integrator' x0 u0 x1 = vzipWith (-) x1' x1
           where
-            x1' = integrator (ocpDae ocp) ts x0 u0 p
-        x1s = tvtail xs |> xf
+            x1' = integrator x0 u0
+        x1s :: Vec n (x a)
+        x1s = TV.tvshiftl xs xf
 
-    objective = (ocpMayer ocp) xf
+    objective = objective' `dot` objective'
+    objective' = (msMayer ocp) xf
+
 --    objectiveL = V.foldl' f 0 (vectorize xus)
 --      where
---        f acc (MsPoint x u) = integrator dae' ts x u p + acc
+--        f acc (MsPoint x u) = integrator dae' ts x u + acc
 --          where
 --            dae' = Dae (\x None u p' -> ocpLagrange ocp x None u p')
 --            ddtL l = ocpLagrange ocp x None u p
---        (ssum (tvzipWith (\x u -> ocpLagrange ocp x None u p) xs us)) / fromIntegral (tvlength us)
+--        (ssum (tvzipWith (\x u -> ocpLagrange ocp x None u p) xs us)) / fromIntegral (tvlength us
+--
 --ssum :: Num a => Vec n a -> a
 --ssum = F.foldl' (+) 0
 
-makeNlp :: (Vectorize x, NaturalT n, PositiveT n, n ~ (Succ (Pred n)), Vectorize bc) =>
-           (forall a. Floating a => OcpPhase x None u p x bc pc a) ->
-           (forall a. Floating a => Integrator x None u p x a) ->
-           Nlp (MsTraj x u p n) (MsConstraints x n bc pc)
-makeNlp ocp integrator = Nlp (getFg ocp integrator) (getDvBnds ocp) (getGBnds ocp)
+makeNlp ::
+  (Vectorize x, Vectorize u, Vectorize c, Dim n) =>
+  MsOcp x u c h ->
+  (forall a. Floating a => x a -> u a -> x a) ->
+  Nlp (MsTraj x u n) None (MsConstraints x n c h)
+makeNlp ocp integrator = Nlp { nlpFG = getFg ocp integrator
+                             , nlpBX = getDvBnds ocp
+                             , nlpBG = getGBnds ocp
+                             , nlpX0 = fill 0
+                             , nlpP = None
+                             }
