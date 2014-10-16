6345f0568380cf5c24d61b6436e7037bf319ad70
Greg Horn <gregmainland@gmail.com>
2014-10-16 14:12:34 +0200

    export outputs/xdots for matlab

--- src/Dyno/DirectCollocation/Export.hs
+++ src/Dyno/DirectCollocation/Export.hs
@@ -14,83 +14,92 @@ import Dyno.Server.Accessors ( Lookup, flatten, accessors )
 import Dyno.TypeVecs ( Vec )
 import Dyno.Vectorize ( Vectorize, Proxy(..), fill )
 import Dyno.View ( View(..), JV, JVec(..), unJ, splitJV )
+import Dyno.DirectCollocation.Formulate ( CollProblem(..) )
 import Dyno.DirectCollocation.Types ( CollTraj(..), CollStage(..), CollPoint(..) )
 import Dyno.DirectCollocation.Quadratures ( timesFromTaus )
 
 toMatlab ::
-  forall x z u p n deg
+  forall x z u p r c h o n deg
   . ( Lookup (x Double), Vectorize x
     , Lookup (z Double), Vectorize z
     , Lookup (u Double), Vectorize u
+    , Lookup (o Double), Vectorize o
     , Lookup (p Double), Vectorize p
     , Dim deg
     , Dim n
     )
-  => Vec deg Double -> CollTraj x z u p n deg (Vector Double) -> String
-toMatlab taus (CollTraj tf' p' stages' xf) = ret
-  where
-    tf = V.head (unJ tf')
+  => CollProblem x z u p r c h o n deg
+  -> CollTraj x z u p n deg (Vector Double)
+  -> IO String
+toMatlab cp ct@(CollTraj tf' p' stages' xf) = do
+  outs <- fmap snd $ cpCallback cp (cat ct)
 
-    times :: Vec n (Double, Vec deg Double)
-    times = timesFromTaus taus Proxy dt
-      where
-        dt = tf / fromIntegral (reflectDim (Proxy :: Proxy n) )
+  let taus = cpTaus cp
+      tf = V.head (unJ tf')
 
-    xTimes = concatMap (\(t0,ts) -> t0 : F.toList ts) (F.toList times) ++ [tf]
-    zuoTimes = concatMap (\(t0,ts) -> t0 : F.toList ts) (F.toList times) ++ [tf]
+      n = reflectDim (Proxy :: Proxy n)
+      deg = reflectDim (Proxy :: Proxy deg)
+      times :: Vec n (Double, Vec deg Double)
+      times = timesFromTaus taus Proxy dt
+        where
+          dt = tf / fromIntegral n
 
-    stages :: [CollStage (JV x) (JV z) (JV u) deg (Vector Double)]
-    stages = map split $ F.toList $ unJVec $ split stages'
+      xTimes = concatMap (\(t0,ts) -> t0 : F.toList ts) (F.toList times) ++ [tf]
+      zuoTimes = concatMap (\(t0,ts) -> t0 : F.toList ts) (F.toList times) ++ [tf]
 
-    xs :: [x Double]
-    xs = concatMap getXs stages ++ [splitJV xf]
+      stages :: [CollStage (JV x) (JV z) (JV u) deg (Vector Double)]
+      stages = map split $ F.toList $ unJVec $ split stages'
 
-    zs :: [z Double]
-    zs = concatMap getZs stages
+      xs :: [x Double]
+      xs = concatMap getXs stages ++ [splitJV xf]
 
-    us :: [u Double]
-    us = concatMap getUs stages
+      zs :: [z Double]
+      zs = concatMap getZs stages
 
-    getXs (CollStage x0 xzus) = splitJV x0 : map (getX . split) (F.toList (unJVec (split xzus)))
-    getZs (CollStage  _ xzus) =              map (getZ . split) (F.toList (unJVec (split xzus)))
-    getUs (CollStage  _ xzus) =              map (getU . split) (F.toList (unJVec (split xzus)))
+      us :: [u Double]
+      us = concatMap getUs stages
 
-    getX :: CollPoint (JV x) (JV z) (JV u) (Vector Double) -> x Double
-    getX (CollPoint x _ _) = splitJV x
+      os :: [o Double]
+      xdots :: [x Double]
+      (os, xdots) = unzip $ F.concatMap F.toList outs
 
-    getZ :: CollPoint (JV x) (JV z) (JV u) (Vector Double) -> z Double
-    getZ (CollPoint _ z _) = splitJV z
+      getXs (CollStage x0 xzus) = splitJV x0 : map (getX . split) (F.toList (unJVec (split xzus)))
+      getZs (CollStage  _ xzus) =              map (getZ . split) (F.toList (unJVec (split xzus)))
+      getUs (CollStage  _ xzus) =              map (getU . split) (F.toList (unJVec (split xzus)))
 
-    getU :: CollPoint (JV x) (JV z) (JV u) (Vector Double) -> u Double
-    getU (CollPoint _ _ u) = splitJV u
+      getX :: CollPoint (JV x) (JV z) (JV u) (Vector Double) -> x Double
+      getX (CollPoint x _ _) = splitJV x
 
-    atX :: [(String, x Double -> Double)]
-    atX = flatten $ accessors (fill 0)
+      getZ :: CollPoint (JV x) (JV z) (JV u) (Vector Double) -> z Double
+      getZ (CollPoint _ z _) = splitJV z
 
-    atZ :: [(String, z Double -> Double)]
-    atZ = flatten $ accessors (fill 0)
+      getU :: CollPoint (JV x) (JV z) (JV u) (Vector Double) -> u Double
+      getU (CollPoint _ _ u) = splitJV u
 
-    atU :: [(String, u Double -> Double)]
-    atU = flatten $ accessors (fill 0)
+      at :: (Vectorize xzu, Lookup (xzu Double)) => [(String, xzu Double -> Double)]
+      at = flatten $ accessors (fill 0)
 
-    atP :: [(String, p Double -> Double)]
-    atP = flatten $ accessors (fill 0)
+      p = splitJV p'
 
-    p = splitJV p'
+      woo :: String -> [xzu Double] -> String -> (xzu Double -> Double) -> String
+      woo topName xzus name get = topName ++ "." ++ name ++ " = " ++ show (map get xzus) ++ ";"
 
-    woo :: String -> [xzu Double] -> String -> (xzu Double -> Double) -> String
-    woo topName xzus name get = topName ++ "." ++ name ++ " = " ++ show (map get xzus) ++ ";"
+      wooP :: String -> (p Double -> Double) -> String
+      wooP name get = "params." ++ name ++ " = " ++ show (get p) ++ ";"
 
-    wooP :: String -> (p Double -> Double) -> String
-    wooP name get = "params." ++ name ++ " = " ++ show (get p) ++ ";"
-
-    ret :: String
-    ret = init $ unlines $
-          map (uncurry (woo "ret.diffStates" xs)) atX ++
-          map (uncurry (woo "ret.algVars" zs)) atZ ++
-          map (uncurry (woo "ret.controls" us)) atU ++
-          map (uncurry wooP) atP ++
-          [ ""
-          , "ret.tx = " ++ show xTimes
-          , "ret.tzuo = " ++ show zuoTimes
-          ]
+      ret :: String
+      ret = init $ unlines $
+            map (uncurry (woo "ret.diffStates" xs)) at ++
+            map (uncurry (woo "ret.diffStateDerivs" xdots)) at ++
+            map (uncurry (woo "ret.algVars" zs)) at ++
+            map (uncurry (woo "ret.controls" us)) at ++
+            map (uncurry (woo "ret.outputs" os)) at ++
+            map (uncurry wooP) at ++
+            [ ""
+            , "ret.tx = " ++ show xTimes
+            , "ret.tzuo = " ++ show zuoTimes
+            , "ret.N = " ++ show n
+            , "ret.deg = " ++ show deg
+            , "ret.collocationRoots = '" ++ show (cpRoots cp) ++ "'"
+            ]
+  return ret
