b43f4c6a8cc07598902efe68fcba17ccab14fb30
Greg Horn <gregmainland@gmail.com>
2014-10-28 16:34:25 +0100

    J unit tests

--- tests/ViewTests.hs
+++ tests/ViewTests.hs
@@ -35,10 +35,10 @@ instance Scheme M2
 instance Scheme M3
 instance Scheme M4
 
-testSplit :: forall f g . (Vectorize f, Vectorize g) => String -> Proxy f -> Proxy g -> Test
-testSplit msg _ _ =
+testSplitM :: forall f g . (Vectorize f, Vectorize g) => String -> Proxy f -> Proxy g -> Test
+testSplitM msg _ _ =
   testGroup msg $ 
-  map (testCase ("testSplits " ++ msg))
+  map (testCase ("testSplitsM " ++ msg))
   [ assertEqual "test vertsplit" (show x0) (show x1)
   , assertEqual "test horzsplit" (show x0) (show x2)
   ]
@@ -52,14 +52,33 @@ testSplit msg _ _ =
     x2 :: M (JV f) (JV g) MX
     x2 = hcat (hsplit x0)
 
+testSplitJ :: forall f . Vectorize f => String -> Proxy f -> Test
+testSplitJ msg _ =
+  testCase ("testSplitsJ " ++ msg) $
+  assertEqual "test vertsplit" (show xj0) (show xj2)
+  where
+    UnsafeM xm0 = ones :: M (JV f) (JV Id) MX
+
+    xj0 :: J (JV f) MX
+    xj0 = mkJ xm0
+
+    xj1 :: JV f MX
+    xj1 = split xj0
+
+    xj2 :: J (JV f) MX
+    xj2 = cat xj1
+
 viewTests :: Test
 viewTests =
   testGroup "view tests"
-    [ testSplit "X/X" (Proxy :: Proxy X) (Proxy :: Proxy X)
-    , testSplit "X/XCompound" (Proxy :: Proxy X) (Proxy :: Proxy XCompound)
-    , testSplit "XCompound/X" (Proxy :: Proxy XCompound) (Proxy :: Proxy X)
-    , testSplit "XCompound/XCompound" (Proxy :: Proxy XCompound) (Proxy :: Proxy XCompound)
-    , testSplit "XCompound/None" (Proxy :: Proxy XCompound) (Proxy :: Proxy None)
-    , testSplit "None/XCompound"  (Proxy :: Proxy None) (Proxy :: Proxy XCompound)
-    , testSplit "None/None"  (Proxy :: Proxy None) (Proxy :: Proxy None)
+    [ testSplitM "X/X" (Proxy :: Proxy X) (Proxy :: Proxy X)
+    , testSplitM "X/XCompound" (Proxy :: Proxy X) (Proxy :: Proxy XCompound)
+    , testSplitM "XCompound/X" (Proxy :: Proxy XCompound) (Proxy :: Proxy X)
+    , testSplitM "XCompound/XCompound" (Proxy :: Proxy XCompound) (Proxy :: Proxy XCompound)
+    , testSplitM "XCompound/None" (Proxy :: Proxy XCompound) (Proxy :: Proxy None)
+    , testSplitM "None/XCompound"  (Proxy :: Proxy None) (Proxy :: Proxy XCompound)
+    , testSplitM "None/None"  (Proxy :: Proxy None) (Proxy :: Proxy None)
+    , testSplitJ "None"  (Proxy :: Proxy None)
+    , testSplitJ "X"  (Proxy :: Proxy X)
+    , testSplitJ "XCompound"  (Proxy :: Proxy XCompound)
     ]
