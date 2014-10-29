721a9254599271e838fce590a1749e45fbce5cbf
Greg Horn <gregmainland@gmail.com>
2014-10-29 13:08:20 +0100

    use quickcheck for unit tests

--- tests/VectorizeTests.hs
+++ tests/VectorizeTests.hs
@@ -1,49 +1,92 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# Language GADTs #-}
-{-# Language RankNTypes #-}
 {-# Language ScopedTypeVariables #-}
+{-# Language GADTs #-}
 {-# Language DeriveFunctor #-}
 {-# Language DeriveGeneric #-}
 
-module VectorizeTests ( Vectorizes(..), vectorizeTests, reifyVects, vects) where
+module VectorizeTests
+       ( Vectorizes(..)
+       , vectorizeTests
+       ) where
 
 import GHC.Generics ( Generic )
 import qualified Data.Vector as V
 import Linear
 
+import Test.QuickCheck
 import Test.Framework ( Test, testGroup )
-import Test.Framework.Providers.HUnit ( testCase )
---import Test.HUnit ( Assertion, assertEqual )
-import Test.HUnit ( assertEqual )
+import Test.Framework.Providers.QuickCheck2 ( testProperty )
 
 import Dyno.Vectorize
 
-data X a = X a (V3 a) a (V2 a) deriving (Show, Eq, Functor, Generic, Generic1)
-data XCompound a = XCompound (X a) (V3 (X a)) a (V2 a) deriving (Show, Eq, Functor, Generic, Generic1)
-instance Vectorize X
-instance Vectorize XCompound
+import Utils
+
+
+data X0 a = X0 a (V3 a) a (V2 a) deriving (Show, Eq, Functor, Generic, Generic1)
+data X1 f g h a = X1 (f a) (V3 (g a)) a (V2 a) a (h a) deriving (Show, Eq, Functor, Generic, Generic1)
+
+
+instance Vectorize X0
+instance (Vectorize f, Vectorize g, Vectorize h) => Vectorize (X1 f g h)
 
 data Vectorizes where
-  Vectorizes :: (Show (f Int), Vectorize f) => String -> Proxy f -> Vectorizes
+  Vectorizes ::
+    (Show (f Int), Eq (f Int), Vectorize f)
+    => { vShrinks :: [Vectorizes]
+       , vName :: String
+       , vProxy :: Proxy f } -> Vectorizes
 
-vects :: [Vectorizes]
-vects =
-  [ Vectorizes "X" (Proxy :: Proxy X)
-  , Vectorizes "XCompound" (Proxy :: Proxy XCompound)
-  , Vectorizes "Id" (Proxy :: Proxy Id)
-  , Vectorizes "None" (Proxy :: Proxy None)
+instance Show Vectorizes where
+  show = vName
+
+instance Arbitrary Vectorizes where
+  arbitrary = oneof [primitives, compounds primitives, compounds (compounds primitives)]
+  shrink = vShrinks
+
+primitives :: Gen Vectorizes
+primitives =
+  elements
+  [ Vectorizes [] "None" (Proxy :: Proxy None)
+  , Vectorizes [] "Id" (Proxy :: Proxy Id)
+  , Vectorizes [] "V0" (Proxy :: Proxy V0)
+  , Vectorizes [] "V1" (Proxy :: Proxy V1)
+  , Vectorizes [] "V2" (Proxy :: Proxy V2)
+  , Vectorizes [] "V3" (Proxy :: Proxy V3)
+  , Vectorizes [] "V4" (Proxy :: Proxy V4)
+  , Vectorizes [] "X0" (Proxy :: Proxy X0)
   ]
 
-reifyVects :: (forall f . (Show (f Int), Vectorize f) => String -> Proxy f -> r) -> Vectorizes -> r
-reifyVects f (Vectorizes x y) = f x y
+compounds :: Gen Vectorizes -> Gen Vectorizes
+compounds genIt = do
+  v1@(Vectorizes _ m1 p1) <- genIt
+  v2@(Vectorizes _ m2 p2) <- genIt
+  v3@(Vectorizes _ m3 p3) <- genIt
+  elements
+    [ Vectorizes
+      { vShrinks = [v1, v2]
+      , vName = "Tuple (" ++ m1 ++ ") (" ++ m2 ++ ")"
+      , vProxy = reproxy2 (Proxy :: Proxy Tuple) p1 p2
+      }
+    , Vectorizes
+      { vShrinks = [v1, v2, v3]
+      , vName = "Triple (" ++ m1 ++ ") (" ++ m2 ++ ") (" ++ m3 ++ ")"
+      , vProxy = reproxy3 (Proxy :: Proxy Triple) p1 p2 p3
+      }
+    , Vectorizes
+      { vShrinks = [v1, v2, v3]
+      , vName = "X1 (" ++ m1 ++ ") (" ++ m2 ++ ") " ++ m3 ++ ")"
+      , vProxy = reproxy3 (Proxy :: Proxy X1) p1 p2 p3
+      }
+    ]
 
 fillInc :: forall x . Vectorize x => x Int
 fillInc = devectorize $ V.fromList $ take (vlength (Proxy :: Proxy x)) [0..]
 
-vectorizeThenDevectorize :: forall x . (Show (x Int), Vectorize x) => String -> Proxy x -> Test
-vectorizeThenDevectorize msg _ =
-  testCase ("vectorizeThenDevectorize " ++ msg) $
-  assertEqual ("vectorizeThenDevectorize " ++ msg)  (show x0) (show x1)
+vectorizeThenDevectorize ::
+  forall x
+  . (Show (x Int), Eq (x Int), Vectorize x)
+  => Proxy x -> Bool
+vectorizeThenDevectorize _ = x0 == x1
   where
     x0 :: x Int
     x0 = fillInc
@@ -51,8 +94,12 @@ vectorizeThenDevectorize msg _ =
     x1 :: x Int
     x1 = devectorize (vectorize x0)
 
+prop_vecThenDevec :: Vectorizes -> Bool
+prop_vecThenDevec (Vectorizes _ _ p) = vectorizeThenDevectorize p
+
 
 vectorizeTests :: Test
 vectorizeTests =
-  testGroup "view tests" $
-  map (reifyVects vectorizeThenDevectorize) vects
+  testGroup "vectorize tests"
+  [ testProperty "vec . devec" prop_vecThenDevec
+  ]
--- tests/ViewTests.hs
+++ tests/ViewTests.hs
@@ -1,24 +1,25 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# Language RankNTypes #-}
 {-# Language ScopedTypeVariables #-}
-{-# Language PolyKinds #-}
-{-# Language DeriveFunctor #-}
-{-# Language DeriveGeneric #-}
 {-# Language GADTs #-}
+{-# Language DeriveGeneric #-}
 
-module ViewTests ( viewTests ) where
+module ViewTests
+       ( Views(..)
+       , viewTests
+       ) where
 
 import GHC.Generics ( Generic )
 
+import Test.QuickCheck
 import Test.Framework ( Test, testGroup )
-import Test.Framework.Providers.HUnit ( testCase )
-import Test.HUnit ( assertEqual )
+import Test.Framework.Providers.QuickCheck2 ( testProperty )
 
 import Dyno.Vectorize
 import Dyno.View
 import Dyno.View.M
 
-import VectorizeTests --( Vectorizes(..), vects )
+import Utils
+import VectorizeTests ( Vectorizes(..) )
 
 data JX0 f a = JX0 (J (JV f) a) (J (JV f) a) deriving (Show, Generic, Generic1)
 instance Vectorize f => View (JX0 f)
@@ -33,39 +34,42 @@ data JX2 f g h a = JX2 (J f a) (J (JTuple g (JV h)) a) (J g a) (J (JV h) a) (J f
 instance (View f, View g, Vectorize h) => View (JX2 f g h)
 ----instance Scheme JX2
 
-views :: [Views]
-views =
-  [ Views ("JX2." ++ m1 ++ "." ++ m2 ++ "." ++ m3) (reproxy3 (Proxy :: Proxy JX2) p1 p2 p3)
-  | Views m1 p1 <- jx1s
-  , Views m2 p2 <- jx1s
-  , Vectorizes m3 p3 <- vects
-  ] ++ jx1s
+instance Arbitrary Views where
+  arbitrary = frequency
+              [ (1, compound)
+              , (2, primitives)
+              ]
+
+compound :: Gen Views
+compound = do
+  vc'@(Vectorizes _ mz pz) <- arbitrary
+  let vc = mkJV vc'
+  vw0@(Views _ mv0 pv0) <- arbitrary
+  vw1@(Views _ mv1 pv1) <- arbitrary
+  elements
+    [ Views [vc] ("JX0 (" ++ mz ++ ")") (reproxy (Proxy :: Proxy JX0) pz)
+    , Views [vc,vw0] ("JX1 (" ++ mz ++ ") (" ++ mv0 ++ ")") (reproxy2 (Proxy :: Proxy JX1) pz pv0)
+    , Views [vc, vw0, vw1] ("JX2 (" ++ mv0 ++ ") (" ++ mv1 ++ ") (" ++ mz ++ ")")
+      (reproxy3 (Proxy :: Proxy JX2) pv0 pv1 pz)
+    ]
+
+mkJV :: Vectorizes -> Views
+mkJV (Vectorizes vs m p) = Views shrinks ("JV (" ++ m ++ ")") (reproxyJV p)
   where
-    jx0s :: [Views]
-    jx0s =
-      [ Views ("JX0." ++ m) (reproxy (Proxy :: Proxy JX0) p)
-      | Vectorizes m p <- vects
-      ] ++
-      [ Views "JNone" (Proxy :: Proxy JNone)
-      , Views "S" (Proxy :: Proxy S)
-      ]
+    shrinks :: [Views]
+    shrinks = map mkJV (concatMap vShrinks vs)
 
-    jx1s :: [Views]
-    jx1s =
-      [ Views ("JX1." ++ m1 ++ "." ++ m2) (reproxy2 (Proxy :: Proxy JX1) p1 p2)
-      | Vectorizes m1 p1 <- vects
-      , Views m2 p2 <- jx0s
-      ] ++ jx0s
+    reproxyJV :: Proxy f -> Proxy (JV f)
+    reproxyJV = const Proxy
 
-    reproxy :: Proxy f -> Proxy g -> Proxy (f g)
-    reproxy _ _ = Proxy
-
-    reproxy2 :: Proxy f -> Proxy g -> Proxy h -> Proxy (f g h)
-    reproxy2 _ _ _ = Proxy
-
-    reproxy3 :: Proxy f -> Proxy g -> Proxy h -> Proxy j -> Proxy (f g h j)
-    reproxy3 _ _ _ _ = Proxy
-
+primitives :: Gen Views
+primitives = do
+  v <- arbitrary
+  elements
+    [ Views [] "JNone" (Proxy :: Proxy JNone)
+    , Views [] "S" (Proxy :: Proxy S)
+    , mkJV v
+    ]
 
 --data M1 a = M1 (M JX JX2 a) deriving (Show, Generic, Generic1)
 --data M2 a = M2 (M JNone JNone a) deriving (Show, Generic, Generic1)
@@ -78,80 +82,60 @@ views =
 --instance Scheme M4
 
 data Views where
-  Views :: View f => String -> Proxy f -> Views
+  Views :: View f =>
+           { vwShrinks :: [Views]
+           , vwName :: String
+           , vwProxy :: Proxy f
+           } -> Views
 instance Show Views where
-  show (Views s _) = s
+  show = vwName
 
-testVSplit :: forall f g . (Vectorize f, View g) => String -> Proxy f -> Proxy g -> Test
-testVSplit msg _ _ =
-  testGroup msg $
-  map (testCase ("testVSplit " ++ msg))
-  [ assertEqual "test vertsplit" (show x0) (show x1)
-  ]
+prop_VSplitVCat :: Vectorizes -> Views -> Bool
+prop_VSplitVCat (Vectorizes _ _ p1) (Views _ _ p2) = test p1 p2
   where
-    x0 :: M (JV f) g MX
-    x0 = ones
+    test :: forall f g . (Vectorize f, View g) => Proxy f -> Proxy g -> Bool
+    test _ _ = show x0 == show x1
+      where
+        x0 :: M (JV f) g MX
+        x0 = ones
 
-    x1 :: M (JV f) g MX
-    x1 = vcat (vsplit x0)
+        x1 :: M (JV f) g MX
+        x1 = vcat (vsplit x0)
 
-
-testHSplit :: forall f g . (View f, Vectorize g) => String -> Proxy f -> Proxy g -> Test
-testHSplit msg _ _ =
-  testGroup msg $
-  map (testCase ("testSplitsM " ++ msg))
-  [ assertEqual "test horzsplit" (show x0) (show x1)
-  ]
+prop_HSplitHCat :: Views -> Vectorizes -> Bool
+prop_HSplitHCat (Views _ _ p1) (Vectorizes _ _ p2) = test p1 p2
   where
-    x0 :: M f (JV g) MX
-    x0 = ones
+    test :: forall f g . (View f, Vectorize g) => Proxy f -> Proxy g -> Bool
+    test _ _ = show x0 == show x1
+      where
+        x0 :: M f (JV g) MX
+        x0 = ones
 
-    x1 :: M f (JV g) MX
-    x1 = hcat (hsplit x0)
+        x1 :: M f (JV g) MX
+        x1 = hcat (hsplit x0)
 
-testSplitJ :: forall f . Vectorize f => String -> Proxy f -> Test
-testSplitJ msg _ =
-  testCase ("testSplitsJ " ++ msg) $
-  assertEqual ("test vertsplit" ++ msg) (show xj0) (show xj2)
+prop_testSplitJ :: Vectorizes -> Bool
+prop_testSplitJ (Vectorizes _ _ p) = test p
   where
-    UnsafeM xm0 = ones :: M (JV f) (JV Id) MX
+    test :: forall f . Vectorize f => Proxy f -> Bool
+    test _ = show xj0 == show xj2
+      where
+        UnsafeM xm0 = ones :: M (JV f) (JV Id) MX
 
-    xj0 :: J (JV f) MX
-    xj0 = mkJ xm0
+        xj0 :: J (JV f) MX
+        xj0 = mkJ xm0
 
-    xj1 :: JV f MX
-    xj1 = split xj0
+        xj1 :: JV f MX
+        xj1 = split xj0
 
-    xj2 :: J (JV f) MX
-    xj2 = cat xj1
+        xj2 :: J (JV f) MX
+        xj2 = cat xj1
 
-jTests :: Test
-jTests =
-  testGroup "J tests"
-  [ testGroup "testSplitJ"
-    [ testSplitJ m p
-    | Vectorizes m p <- vects
-    ]
-  ]
-
-mTests :: Test
-mTests =
-  testGroup "M tests"
-  [ testGroup "testVSplit"
-    [ testVSplit (m1 ++ "/" ++ m2) p1 p2
-    | Vectorizes m1 p1 <- vects
-    , Views m2 p2 <- views
-    ]
-  , testGroup "testHSplit"
-    [ testHSplit (m1 ++ "/" ++ m2) p1 p2
-    | Views m1 p1 <- views
-    , Vectorizes m2 p2 <- vects
-    ]
-  ]
 
 viewTests :: Test
 viewTests =
   testGroup "view tests"
-  [ jTests
-  , mTests
+  [ testProperty "vcat . vsplit" prop_VSplitVCat
+  , testProperty "hcat . hsplit" prop_HSplitHCat
+  , testProperty "split . cat J" prop_testSplitJ
   ]
