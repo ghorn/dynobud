98ea78eccb10b0506d4deb78ab652965613075d0
Greg Horn <gregmainland@gmail.com>
2014-10-29 15:08:13 +0100

    add missing Utils file

---
+++ tests/Utils.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# Language PolyKinds #-}
+
+module Utils
+       ( reproxy
+       , reproxy2
+       , reproxy3
+       ) where
+
+import Data.Proxy
+
+reproxy :: Proxy f -> Proxy g -> Proxy (f g)
+reproxy _ _ = Proxy
+
+reproxy2 :: Proxy f -> Proxy g -> Proxy h -> Proxy (f g h)
+reproxy2 _ _ _ = Proxy
+
+reproxy3 :: Proxy f -> Proxy g -> Proxy h -> Proxy j -> Proxy (f g h j)
+reproxy3 _ _ _ _ = Proxy
+
