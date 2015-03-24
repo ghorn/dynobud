b9c6d7ef6c545dbe08bc5862da157db31735321d
Greg Horn <gregmainland@gmail.com>
2015-03-24 17:21:20 +0100

    interpolate collocation constraints

--- src/Dyno/DirectCollocation/Interpolate.hs
+++ src/Dyno/DirectCollocation/Interpolate.hs
@@ -7,6 +7,7 @@
 
 module Dyno.DirectCollocation.Interpolate
        ( interpolateTraj
+       , interpolateConstraints
        ) where
 
 import qualified Data.Traversable as T
@@ -14,26 +15,27 @@ import Data.Proxy ( Proxy(..) )
 import qualified Data.Vector as V
 import qualified Data.Foldable as F
 import Linear.V
+import Linear ( lerp )
 
 import Dyno.View.Unsafe.View ( unJ, mkJ )
 import Dyno.View.View ( View(..), J )
-import Dyno.View.JV ( JV, splitJV )
+import Dyno.View.JV ( JV )
 import Dyno.View.JVec
 import Dyno.TypeVecs ( Vec )
-import Dyno.Vectorize ( Vectorize, Id(..) )
+import Dyno.Vectorize ( Vectorize )
 import qualified Dyno.TypeVecs as TV
 import qualified Dyno.LagrangePolynomials as LP
-import Dyno.DirectCollocation.Types ( CollTraj(..), CollStage(..), CollPoint(..) )
+import Dyno.DirectCollocation.Types ( CollTraj(..), CollStage(..), CollPoint(..), CollOcpConstraints(..) )
 import Dyno.DirectCollocation.Quadratures ( QuadratureRoots, timesFromTaus, mkTaus )
 
 
-type TimeVal f deg = (Double, Vec deg (Double, f), Double)
+type TimeVal f deg = (Double, Vec deg (Double, J f (V.Vector Double)), Double)
 
 -- a zipper to avoid quadratic lookup
 data TimeZ deg f = TimeZ [TimeVal f deg] (TimeVal f deg) [TimeVal f deg]
 
 
-closestTime :: TimeZ deg f -> Double -> (TimeZ deg f, Vec deg (Double, f))
+closestTime :: TimeZ deg f -> Double -> (TimeZ deg f, Vec deg (Double, J f (V.Vector Double)))
 -- time too big and we have another value
 closestTime (TimeZ ls mid@(_, _, t1) (r:rs)) t
   | t > t1 = closestTime (TimeZ (mid:ls) r rs) t
@@ -48,25 +50,24 @@ closestTime tz@(TimeZ _ (t0, xs, _) _) t
 
 
 interp ::
-  forall x z u deg
-  . (Vectorize x, Vectorize z, Vectorize u, Dim deg)
-  => TimeZ deg (Point x z u) -> Double -> (TimeZ deg (Point x z u), Point x z u)
+  forall f deg
+  . (Dim deg, View f)
+  => TimeZ deg f -> Double -> (TimeZ deg f, J f (V.Vector Double))
 interp tz0 t = (tz, ret)
   where
-    txs :: Vec deg (Double, Point x z u)
+    txs :: Vec deg (Double, J f (V.Vector Double))
     (tz, txs) = closestTime tz0 t
 
     ts :: Vec deg Double
-    xs :: Vec deg (Point x z u)
+    xs :: Vec deg (J f (V.Vector Double))
     (ts,xs) = TV.tvunzip txs
 
-    ret :: Point x z u
+    ret :: J f (V.Vector Double)
     ret = mkJ $ LP.interpolate ts (fmap unJ xs) t
 
 
-type Point x z u = J (CollPoint (JV x) (JV z) (JV u)) (V.Vector Double)
+type Point x z u = CollPoint (JV x) (JV z) (JV u)
 newtype Times deg a = Times (a, Vec deg a) deriving (Functor, F.Foldable, T.Traversable)
-
 
 -- | re-discretize a collocation trajectory using the lagrange interpolation polynomials
 -- from the quadrature scheme
@@ -76,15 +77,15 @@ interpolateTraj ::
     , Vectorize x, Vectorize z, Vectorize u
     )
   => Vec deg0 Double
+  -> Double
   -> CollTraj x z u p n0 deg0 (V.Vector Double)
   -> QuadratureRoots
   -> CollTraj x z u p n1 deg1 (V.Vector Double)
-interpolateTraj taus0 traj0 roots1 = traj0 { ctStages = cat (JVec (fmap cat stages1)) }
+interpolateTraj taus0 tf traj0 roots1 = traj0 { ctStages = cat (JVec (fmap cat stages1)) }
   where
     n0 = reflectDim (Proxy :: Proxy n0)
     n1 = reflectDim (Proxy :: Proxy n1)
 
-    Id tf = splitJV (ctTf traj0)
     dt0 = tf / fromIntegral n0
     dt1 = tf / fromIntegral n1
 
@@ -124,3 +125,85 @@ interpolateTraj taus0 traj0 roots1 = traj0 { ctStages = cat (JVec (fmap cat stag
         CollPoint x0 _ _ = split xzu0
         (timez1, xzu0) = interp timez0 t0
         (timezf, xzus) = T.mapAccumL interp timez1 ts
+
+
+-- | Re-discretize collocation constraints using the lagrange interpolation polynomials
+-- from the quadrature scheme. This is useful for lagrange multipliers.
+interpolateConstraints ::
+  forall x r c h n0 n1 deg0 deg1
+  . ( Dim n0, Dim n1, Dim deg0, Dim deg1
+    , Vectorize x, Vectorize r, Vectorize c, Vectorize h
+    )
+  => Vec deg0 Double
+  -> Double
+  -> CollOcpConstraints x r c h n0 deg0 (V.Vector Double)
+  -> QuadratureRoots
+  -> CollOcpConstraints x r c h n1 deg1 (V.Vector Double)
+interpolateConstraints taus0 tf con0 roots1 = con1
+  where
+    con1 = CollOcpConstraints
+           { coCollPoints =  go' (coCollPoints con0)
+           , coPathC = go' (coPathC con0)
+           , coContinuity = cat (JVec cont)
+           , coBc = coBc con0
+           }
+
+    cont0 :: Vec n0 (J (JV x) (V.Vector Double))
+    cont0 = unJVec $ split (coContinuity con0)
+
+    tc0 :: Vec n0 (Double, J (JV x) (V.Vector Double))
+    tc0 = TV.tvzip (fmap fst times0') cont0
+
+    cont :: Vec n1 (J (JV x) (V.Vector Double))
+    cont = snd $ T.mapAccumL linterp (F.toList tc0) (fmap fst times1)
+
+    go' :: forall s
+           . Vectorize s
+           => J (JVec n0 (JVec deg0 (JV s))) (V.Vector Double)
+           -> J (JVec n1 (JVec deg1 (JV s))) (V.Vector Double)
+    go' x = cat $ JVec $ fmap (cat . JVec) (go (fmap (unJVec . split) (unJVec (split x))))
+
+    n0 = reflectDim (Proxy :: Proxy n0)
+    n1 = reflectDim (Proxy :: Proxy n1)
+
+    dt0 = tf / fromIntegral n0
+    dt1 = tf / fromIntegral n1
+
+    taus1 :: Vec deg1 Double
+    taus1 = mkTaus roots1
+
+    times0' :: Vec n0 (Double, Vec deg0 Double)
+    times0' = timesFromTaus 0 taus0 dt0
+
+    times1 :: Vec n1 (Double, Vec deg1 Double)
+    times1 = timesFromTaus 0 taus1 dt1
+
+    go :: forall s
+          . View s
+          => Vec n0 (Vec deg0 (J s (V.Vector Double)))
+          -> Vec n1 (Vec deg1 (J s (V.Vector Double)))
+    go x0s = x1s
+      where
+        x1s :: Vec n1 (Vec deg1 (J s (V.Vector Double)))
+        x1s = snd $ T.mapAccumL (T.mapAccumL interp) tzip0' (fmap snd times1)
+
+        tzip0' :: TimeZ deg0 s
+        tzip0' = case F.toList times00 of
+          [] -> error "can't interpolate with 0 length input"
+          (x:xs) -> TimeZ [] x xs
+
+        times00 :: Vec n0 (TimeVal s deg0)
+        times00 = TV.tvzipWith3 (\(t0,ts) t1 xs -> (t0, TV.tvzip ts xs, t1))
+                  times0' (TV.tvshiftl (fmap fst times0') tf) x0s
+
+
+
+linterp :: View s
+           => [(Double, J s (V.Vector Double))]
+           -> Double
+           -> ([(Double, J s (V.Vector Double))], J s (V.Vector Double))
+-- if t is too big and there are others available
+linterp (_:others@((t1,_):_:_)) t
+  | t > t1 = linterp others t
+linterp acc@((t0,x0):(t1,x1):_) t = (acc, mkJ (lerp ((t - t0) / (t1 - t0)) (unJ x0) (unJ x1)))
+linterp _ _ = error "linear interpolation ran out of nodes"
