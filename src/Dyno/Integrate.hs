9e54db1b844093f61796ae279020d3c1906a0c14
Greg Horn <gregmainland@gmail.com>
2016-05-07 14:12:55 -0700

    Make InitialTime ans TimeStep Functors.

--- src/Dyno/Integrate.hs
+++ src/Dyno/Integrate.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Dyno.Integrate
@@ -8,6 +10,11 @@ module Dyno.Integrate
        , rk45
        ) where
 
+import GHC.Generics ( Generic, Generic1 )
+
+import Accessors ( Lookup(..) )
+import Data.Aeson ( FromJSON(..), ToJSON(..) )
+import Data.Serialize ( Serialize )
 import qualified Data.Vector as V
 import qualified Data.Vector.Storable as SV
 import qualified Numeric.GSL.ODE as ODE
@@ -15,14 +22,28 @@ import qualified Numeric.LinearAlgebra.Data as D
 
 import Dyno.Vectorize ( Vectorize(..), devectorize )
 
-newtype InitialTime = InitialTime {unInitialTime :: Double}
-                    deriving (Num, Fractional, Floating, Ord, Eq, Show)
-newtype TimeStep = TimeStep {unTimeStep :: Double}
-                 deriving (Num, Fractional, Floating, Ord, Eq, Show)
+newtype InitialTime a = InitialTime {unInitialTime :: a}
+  deriving (Functor, Generic, Generic1, FromJSON, ToJSON, Num, Fractional, Floating, Ord, Eq, Show)
+instance Vectorize InitialTime
+instance Lookup a => Lookup (InitialTime a) where
+  toAccessorTree lens0 = toAccessorTree (lens0 . lens1)
+    where
+      lens1 f y = fmap InitialTime (f (unInitialTime y))
+instance Serialize a => Serialize (InitialTime a)
+
+
+newtype TimeStep a = TimeStep {unTimeStep :: a}
+  deriving (Functor, Generic, Generic1, FromJSON, ToJSON, Num, Fractional, Floating, Ord, Eq, Show)
+instance Vectorize TimeStep
+instance Lookup a => Lookup (TimeStep a) where
+  toAccessorTree lens0 = toAccessorTree (lens0 . lens1)
+    where
+      lens1 f y = fmap TimeStep (f (unTimeStep y))
+instance Serialize a => Serialize (TimeStep a)
 
 rk45 :: Vectorize x
         => (Double -> x Double -> x Double)
-        -> InitialTime -> TimeStep -> x Double -> x Double
+        -> InitialTime Double -> TimeStep Double -> x Double -> x Double
 rk45 f (InitialTime t0) (TimeStep h) x0 = devectorize $ sv $ last sol
   where
     vs :: V.Vector Double -> SV.Vector Double
