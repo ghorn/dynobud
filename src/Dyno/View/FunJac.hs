7170d82aea20a45af3cc0179d71acbfdeb766d7f
Greg Horn <gregmainland@gmail.com>
2017-01-08 16:46:42 -0800

    use ScopedTypeVariables instead of reproxy to clarify intent

--- src/Dyno/View/FunJac.hs
+++ src/Dyno/View/FunJac.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Dyno.View.FunJac
        ( JacIn(..)
@@ -20,10 +21,7 @@ data HessOut f a = HessOut (S a) (f a) deriving Show
 data Hess xj f a = Hess (M xj xj a) (J xj a) (S a) (f a) deriving Show
 
 instance (View xj, Scheme x) => Scheme (JacIn xj x) where
-  numFields = (1+) . numFields . reproxy
-    where
-      reproxy :: Proxy (JacIn xj x) -> Proxy x
-      reproxy = const Proxy
+  numFields = const $ 1 + numFields (Proxy :: Proxy x)
   fromVector v = JacIn j0 (fromVector (V.tail v))
     where
       j0 = case fromFioMat (V.head v) of
@@ -31,18 +29,10 @@ instance (View xj, Scheme x) => Scheme (JacIn xj x) where
         Right j0' -> j0'
 
   toVector (JacIn xj x) = V.cons (toFioMat xj) (toVector x)
-  sizeList p = fioMatSizes (reproxy' p) : sizeList (reproxy p)
-    where
-      reproxy :: Proxy (JacIn xj x) -> Proxy x
-      reproxy = const Proxy
-      reproxy' :: Proxy (JacIn xj x) -> Proxy (J xj)
-      reproxy' = const Proxy
+  sizeList = const $ fioMatSizes (Proxy :: Proxy (J xj)) : sizeList (Proxy :: Proxy x)
 
 instance (View fj, Scheme f) => Scheme (JacOut fj f) where
-  numFields = (1+) . numFields . reproxy
-    where
-      reproxy :: Proxy (JacOut fj f) -> Proxy f
-      reproxy = const Proxy
+  numFields = const $ 1 + numFields (Proxy :: Proxy f)
   fromVector v = JacOut j0 (fromVector (V.tail v))
     where
       j0 = case fromFioMat (V.head v) of
@@ -50,18 +40,10 @@ instance (View fj, Scheme f) => Scheme (JacOut fj f) where
         Right j0' -> j0'
 
   toVector (JacOut fj f) = V.cons (toFioMat fj) (toVector f)
-  sizeList p = fioMatSizes (reproxy' p) : sizeList (reproxy p)
-    where
-      reproxy :: Proxy (JacOut fj f) -> Proxy f
-      reproxy = const Proxy
-      reproxy' :: Proxy (JacOut fj f) -> Proxy (J fj)
-      reproxy' = const Proxy
+  sizeList = const $ fioMatSizes (Proxy :: Proxy (J fj)) : sizeList (Proxy :: Proxy f)
 
 instance Scheme f => Scheme (HessOut f) where
-  numFields = (1+) . numFields . reproxy
-    where
-      reproxy :: Proxy (HessOut f) -> Proxy f
-      reproxy = const Proxy
+  numFields = const $ 1 + numFields (Proxy :: Proxy f)
   fromVector v = HessOut h0 (fromVector (V.tail v))
     where
       h0 = case fromFioMat (V.head v) of
@@ -69,19 +51,11 @@ instance Scheme f => Scheme (HessOut f) where
         Right r -> r
 
   toVector (HessOut s f) = V.cons (toFioMat s) (toVector f)
-  sizeList p = fioMatSizes (reproxy' p) : sizeList (reproxy p)
-    where
-      reproxy' :: Proxy (HessOut f) -> Proxy S
-      reproxy' = const Proxy
-      reproxy :: Proxy (HessOut f) -> Proxy f
-      reproxy = const Proxy
+  sizeList = const $ fioMatSizes (Proxy :: Proxy S) : sizeList (Proxy :: Proxy f)
 
 
 instance (View xj, View fj, Scheme f) => Scheme (Jac xj fj f) where
-  numFields = (2+) . numFields . reproxy
-    where
-      reproxy :: Proxy (Jac xj fj f) -> Proxy f
-      reproxy = const Proxy
+  numFields = const $ 2 + numFields (Proxy :: Proxy f)
   fromVector v = Jac m fj (fromVector (V.drop 2 v))
     where
       m = case fromFioMat (v V.! 0) of
@@ -91,23 +65,14 @@ instance (View xj, View fj, Scheme f) => Scheme (Jac xj fj f) where
         Left err -> error $ "Jac fromVector error: " ++ err
         Right j0' -> j0'
   toVector (Jac m fj f) = V.fromList [toFioMat m, toFioMat fj] V.++ toVector f
-  sizeList p = fioMatSizes (reproxy'' p) : fioMatSizes (reproxy' p) : sizeList (reproxy p)
-    where
-      reproxy'' :: Proxy (Jac xj fj f) -> Proxy (M fj xj)
-      reproxy'' = const Proxy
-
-      reproxy' :: Proxy (Jac xj fj f) -> Proxy (J fj)
-      reproxy' = const Proxy
-
-      reproxy :: Proxy (Jac xj fj f) -> Proxy f
-      reproxy = const Proxy
+  sizeList = const $
+      fioMatSizes (Proxy :: Proxy (M fj xj))
+    : fioMatSizes (Proxy :: Proxy (J fj))
+    : sizeList (Proxy :: Proxy f)
 
 
 instance (View xj, Scheme f) => Scheme (Hess xj f) where
-  numFields = (3+) . numFields . reproxy
-    where
-      reproxy :: Proxy (Hess xj f) -> Proxy f
-      reproxy = const Proxy
+  numFields = const $ 3 + numFields (Proxy :: Proxy f)
   fromVector v = Hess hess jac fun (fromVector (V.drop 3 v))
     where
       hess = case fromFioMat (v V.! 0) of
@@ -120,20 +85,8 @@ instance (View xj, Scheme f) => Scheme (Hess xj f) where
         Left err -> error $ "Hess fromVector error: " ++ err
         Right r -> r
   toVector (Hess hess jac fun f) = V.fromList [toFioMat hess, toFioMat jac, toFioMat fun] V.++ toVector f
-  sizeList p =
-      fioMatSizes (reproxy''' p)
-    : fioMatSizes (reproxy'' p)
-    : fioMatSizes (reproxy' p)
-    : sizeList (reproxy p)
-    where
-      reproxy''' :: Proxy (Hess xj f) -> Proxy (M xj xj)
-      reproxy''' = const Proxy
-
-      reproxy'' :: Proxy (Hess xj f) -> Proxy (J xj)
-      reproxy'' = const Proxy
-
-      reproxy' :: Proxy (Hess xj f) -> Proxy S
-      reproxy' = const Proxy
-
-      reproxy :: Proxy (Hess xj f) -> Proxy f
-      reproxy = const Proxy
+  sizeList = const $
+      fioMatSizes (Proxy :: Proxy (M xj xj))
+    : fioMatSizes (Proxy :: Proxy (J xj))
+    : fioMatSizes (Proxy :: Proxy S)
+    : sizeList (Proxy :: Proxy f)
