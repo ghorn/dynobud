3fa17711a6179d61be49d53f1bf34dcf53a460af
Greg Horn <gregmainland@gmail.com>
2017-02-28 17:23:36 -0800

    Implement interp3, I think

--- src/Dyno/View/Interpolant.hs
+++ src/Dyno/View/Interpolant.hs
@@ -3,12 +3,12 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Dyno.View.Interpolant
-       ( interpolant1, interpolant2
+       ( interpolant1, interpolant2, interpolant3
        ) where
 
 import Data.Map ( Map )
 import qualified Data.Vector as V
-import Linear ( V2 )
+import Linear ( V2, V3 )
 
 import qualified Casadi.Interpolant as C
 import Casadi.GenericType ( GType )
@@ -36,8 +36,36 @@ interpolant2 name solver grid0 grid1 values0 opts = case checkFunDimensions fun 
   Left err -> error $ "error making interpolant2 " ++ name ++ ": " ++ err
   where
     -- transpose values
+    values :: g (f Double)
     values = sequenceA values0
 
+    vectorizedValues :: V.Vector Double
+    vectorizedValues = V.concatMap vectorize (vectorize values)
+
     fun :: Fun (J (JV V2)) S
-    fun = Fun $ C.interpolant name solver grid (V.concatMap vectorize (vectorize values)) opts
+    fun = Fun $ C.interpolant name solver grid vectorizedValues opts
     grid = V.fromList [vectorize grid0, vectorize grid1]
+
+interpolant3 ::
+  forall f g h
+  . ( Vectorize f, Traversable f
+    , Vectorize g, Applicative g, Traversable g
+    , Vectorize h, Applicative h
+    )
+  => String -> String -> f Double -> g Double -> h Double -> f (g (h Double)) -> Map String GType
+  -> Fun (J (JV V3)) S
+interpolant3 name solver grid0 grid1 grid2 values0 opts = case checkFunDimensions fun of
+  Right _ -> fun
+  Left err -> error $ "error making interpolant3 " ++ name ++ ": " ++ err
+  where
+    -- transpose values
+    values :: h (g (f Double))
+    values = sequenceA $ fmap sequenceA (sequenceA values0)
+    -- values = fmap sequenceA $ sequenceA (fmap sequenceA values0)
+
+    vectorizedValues :: V.Vector Double
+    vectorizedValues = V.concatMap vectorize $ V.concatMap vectorize (vectorize values)
+
+    fun :: Fun (J (JV V3)) S
+    fun = Fun $ C.interpolant name solver grid vectorizedValues opts
+    grid = V.fromList [vectorize grid0, vectorize grid1, vectorize grid2]
--- tests/InterpolantTests.hs
+++ tests/InterpolantTests.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 
 module InterpolantTests
        ( interpolantTests
@@ -7,7 +8,7 @@ module InterpolantTests
 
 import Control.Compose ( (:.)(..) )
 import qualified Data.Vector as V
-import Linear ( V2(..), V3(..) )
+import Linear ( V2(..), V3(..), V4(..) )
 
 import qualified Test.HUnit.Base as HUnit
 import Test.Framework ( Test, testGroup )
@@ -21,6 +22,9 @@ call1 f x = (unId . splitJV) <$> callV f (catJV (Id x))
 
 call2 :: Fun (J (JV V2)) S -> Double -> Double -> IO Double
 call2 f x0 x1 = (unId . splitJV) <$> callV f (catJV (V2 x0 x1))
+
+call3 :: Fun (J (JV V3)) S -> Double -> Double -> Double -> IO Double
+call3 f x0 x1 x2 = (unId . splitJV) <$> callV f (catJV (V3 x0 x1 x2))
 
 test_knots_interpolant1 :: HUnit.Assertion
 test_knots_interpolant1 = HUnit.assert $ do
@@ -84,6 +88,57 @@ test_in_between_interpolant2 = HUnit.assert $ do
   return $ HUnit.assertEqual "" (fmap snd expected) newValues
 
 
+test_knots_interpolant3 :: HUnit.Assertion
+test_knots_interpolant3 = HUnit.assert $ do
+  let grid0 :: V2 Double
+      grid0 = V2 10 15
+
+      grid1 :: V3 Double
+      grid1 = V3 1 3 5
+
+      grid2 :: V4 Double
+      grid2 = V4 100 200 300 400
+
+      values :: V2 (V3 (V4 Double))
+      values =
+        V2
+        (V3 (V4 1 2 3 4) (V4 5 6 7 8) (V4 9 10 11 12))
+        (V3 (V4 13 14 15 16) (V4 17 18 19 20) (V4 21 22 23 34))
+
+      f = interpolant3 "woo" "linear" grid0 grid1 grid2 values mempty
+
+      xs :: V2 (V3 (V4 Double))
+      xs = fmap (fmap pure) $ fmap pure grid0
+
+      ys :: V2 (V3 (V4 Double))
+      ys = pure $ fmap pure grid1
+
+      zs :: V2 (V3 (V4 Double))
+      zs = pure $ pure grid2
+
+  O (O newValues) <- sequenceA (call3 f <$> O (O xs) <*> O (O ys) <*> O (O zs))
+
+  return $ HUnit.assertEqual "" values newValues
+
+-- test_in_between_interpolant2 :: HUnit.Assertion
+-- test_in_between_interpolant2 = HUnit.assert $ do
+--   let grid0 :: V2 Double
+--       grid0 = V2 10 15
+--
+--       grid1 :: V3 Double
+--       grid1 = V3 1 3 5
+--
+--       values = V2 (V3 10 11 12) (V3 20 21 22)
+--
+--       f = interpolant2 "woo" "linear" grid0 grid1 values mempty
+--
+--       expected = V.fromList [((12.5, 1), 15), ((15, 4), 21.5)]
+--
+--   newValues <- mapM (uncurry (call2 f)) (fmap fst expected)
+--
+--   return $ HUnit.assertEqual "" (fmap snd expected) newValues
+
+
 interpolantTests :: Test
 interpolantTests =
   testGroup "interpolant tests"
@@ -91,4 +146,6 @@ interpolantTests =
   , testCase "interp1 in between" test_in_between_interpolant1
   , testCase "interp2 knots" test_knots_interpolant2
   , testCase "interp2 in between" test_in_between_interpolant2
+  , testCase "interp3 knots" test_knots_interpolant3
+  --, testCase "interp3 in between" test_in_between_interpolant3
   ]
