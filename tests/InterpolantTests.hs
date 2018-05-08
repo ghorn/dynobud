63f69e3af8efc30384e885c87841526fd295bf01
Greg Horn <greg@kittyhawk.aero>
2018-05-07 19:48:54 -0700

    Test multi dimensional output lookup tables

--- tests/InterpolantTests.hs
+++ tests/InterpolantTests.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GADTs #-}
 
 module InterpolantTests
        ( interpolantTests
@@ -25,66 +26,66 @@ import qualified Test.HUnit.Base as HUnit
 import Test.Framework ( Test, testGroup )
 import Test.Framework.Providers.HUnit ( testCase )
 
-import Dyno.View ( Vectorize, Fun, J, JV, S, Id(..), None(..)
+import Dyno.View ( Vectorize, Fun, Id(..), J, JV, S, None(..)
                  , toFun, callDM, vcountUp, catJV, splitJV, unId, d2v, v2d, vcat )
 import Dyno.View.Interpolant ( Interpolant(..) )
 import Dyno.View.Unsafe ( M(UnsafeM) )
 
-sndId :: (a, b) -> (a, Id b)
-sndId (x, y) = (x, Id y)
+overSnd :: (b -> f b) -> (a, b) -> (a, f b)
+overSnd f (x, y) = (x, f y)
 
-test_knots_interpolant1 :: forall a .
-                           Interpolant a
-                        => String -> (a -> Double) -> (Double -> a)
+test_knots_interpolant1 :: forall f a .
+                           (Vectorize f, Interpolant a, Eq (f Double), Show (f Double))
+                        => String -> (Double -> f Double) -> (a -> Double) -> (Double -> a)
                         -> HUnit.Assertion
-test_knots_interpolant1 solver toDouble fromDouble = HUnit.assert $ do
-  let gridAndValues :: V.Vector (Double, Double)
-      gridAndValues = V.fromList [(1, 10), (2, 20), (3, 30)]
+test_knots_interpolant1 solver toMulti toDouble fromDouble = HUnit.assert $ do
+  let gridAndValues :: V.Vector (Double, f Double)
+      gridAndValues = overSnd toMulti <$> V.fromList [(1, 10), (2, 20), (3, 30)]
 
-  f <- makeInterpolant1 "test_knots_interpolant1" solver (fmap sndId gridAndValues)
-       :: IO (a -> Id a)
+  f <- makeInterpolant1 "test_knots_interpolant1" solver (gridAndValues)
+       :: IO (a -> f a)
 
-  let newValues :: V.Vector Double
-      newValues = fmap (toDouble . unId . f . fromDouble) (fmap fst gridAndValues)
+  let newValues :: V.Vector (f Double)
+      newValues = fmap (fmap toDouble . f . fromDouble) (fmap fst gridAndValues)
 
   return $ HUnit.assertEqual "" (fmap snd gridAndValues) newValues
 
-test_in_between_interpolant1 :: forall a .
-                                Interpolant a
-                             => String -> (a -> Double) -> (Double -> a)
+test_in_between_interpolant1 :: forall f a .
+                                (Vectorize f, Interpolant a, Eq (f Double), Show (f Double))
+                             => String -> (Double -> f Double) -> (a -> Double) -> (Double -> a)
                              -> HUnit.Assertion
-test_in_between_interpolant1 solver toDouble fromDouble = HUnit.assert $ do
-  let gridAndValues :: V.Vector (Double, Double)
-      gridAndValues = V.fromList [(1, 10), (2, 20), (3, 30)]
+test_in_between_interpolant1 solver toMulti toDouble fromDouble = HUnit.assert $ do
+  let gridAndValues :: V.Vector (Double, f Double)
+      gridAndValues = overSnd toMulti <$> V.fromList [(1, 10), (2, 20), (3, 30)]
 
-  f <- makeInterpolant1 "test_in_between_interpolant1" solver (fmap sndId gridAndValues)
-       :: IO (a -> Id a)
+  f <- makeInterpolant1 "test_in_between_interpolant1" solver gridAndValues
+       :: IO (a -> f a)
 
-  let expected :: V.Vector (Double, Double)
-      expected = V.fromList [(1.5, 15), (1.7, 17), (2.5, 25), (3.2, 32)]
+  let expected :: V.Vector (Double, f Double)
+      expected = overSnd toMulti <$> V.fromList [(1.5, 15), (1.7, 17), (2.5, 25), (3.2, 32)]
 
-      newValues :: V.Vector Double
-      newValues = fmap (toDouble . unId . f . fromDouble) (fmap fst expected)
+      newValues :: V.Vector (f Double)
+      newValues = fmap (fmap toDouble . f . fromDouble) (fmap fst expected)
 
   return $ HUnit.assertEqual "" (fmap snd expected) newValues
 
 
-test_knots_interpolant2 :: forall a .
-                           Interpolant a
-                        => String -> (a -> Double) -> (Double -> a)
+test_knots_interpolant2 :: forall f a .
+                           (Vectorize f, Interpolant a, Eq (f Double), Show (f Double))
+                        => String -> (Double -> f Double) -> (a -> Double) -> (Double -> a)
                         -> HUnit.Assertion
-test_knots_interpolant2 solver toDouble fromDouble = HUnit.assert $ do
+test_knots_interpolant2 solver toMulti toDouble fromDouble = HUnit.assert $ do
   let grid0 :: V2 Double
       grid0 = V2 10 15
 
       grid1 :: V3 Double
       grid1 = V3 1 3 5
 
-      values :: V2 (V3 Double)
-      values = V2 (V3 10 11 12) (V3 20 21 22)
+      values :: V2 (V3 (f Double))
+      values = (unO . fmap toMulti . O) $ V2 (V3 10 11 12) (V3 20 21 22)
 
-  f <- makeInterpolant2 "test_knots_interpolant2" solver grid0 grid1 ((unO . fmap Id . O) values)
-       :: IO (V2 a -> Id a)
+  f <- makeInterpolant2 "test_knots_interpolant2" solver grid0 grid1 values
+       :: IO (V2 a -> f a)
 
   let xs :: V2 (V3 Double)
       xs = fmap pure grid0
@@ -92,41 +93,42 @@ test_knots_interpolant2 solver toDouble fromDouble = HUnit.assert $ do
       ys :: V2 (V3 Double)
       ys = pure grid1
 
-      newValues :: V2 (V3 Double)
-      O newValues = fmap (toDouble . unId . f . fmap fromDouble) (V2 <$> O xs <*> O ys)
+      newValues :: V2 (V3 (f Double))
+      O newValues = fmap (fmap toDouble . f . fmap fromDouble) (V2 <$> O xs <*> O ys)
 
   return $ HUnit.assertEqual "" values newValues
 
 
-test_in_between_interpolant2 :: forall a .
-                                Interpolant a
-                             => String -> (a -> Double) -> (Double -> a)
+test_in_between_interpolant2 :: forall f a .
+                                (Vectorize f, Interpolant a, Eq (f Double), Show (f Double))
+                             => String -> (Double -> f Double) -> (a -> Double) -> (Double -> a)
                              -> HUnit.Assertion
-test_in_between_interpolant2 solver toDouble fromDouble = HUnit.assert $ do
+test_in_between_interpolant2 solver toMulti toDouble fromDouble = HUnit.assert $ do
   let grid0 :: V2 Double
       grid0 = V2 10 15
 
       grid1 :: V3 Double
       grid1 = V3 1 3 5
 
-      values = V2 (V3 10 11 12) (V3 20 21 22)
+      values :: V2 (V3 (f Double))
+      values = (unO . fmap toMulti . O) $ V2 (V3 10 11 12) (V3 20 21 22)
 
-  f <- makeInterpolant2 "test_in_between_interpolant2" solver grid0 grid1 ((unO . fmap Id . O) values)
-       :: IO (V2 a -> Id a)
+  f <- makeInterpolant2 "test_in_between_interpolant2" solver grid0 grid1 values
+       :: IO (V2 a -> f a)
 
-  let expected :: V.Vector (V2 Double, Double)
-      expected = V.fromList [(V2 12.5 1, 15), (V2 15 4, 21.5)]
+  let expected :: V.Vector (V2 Double, f Double)
+      expected = fmap (overSnd toMulti) $ V.fromList [(V2 12.5 1, 15), (V2 15 4, 21.5)]
 
-      newValues :: V.Vector Double
-      newValues = fmap (toDouble . unId . f . fmap fromDouble) (fmap fst expected)
+      newValues :: V.Vector (f Double)
+      newValues = fmap (fmap toDouble . f . fmap fromDouble) (fmap fst expected)
 
   return $ HUnit.assertEqual "" (fmap snd expected) newValues
 
-test_knots_interpolant3 :: forall a .
-                           Interpolant a
-                        => String -> (a -> Double) -> (Double -> a)
+test_knots_interpolant3 :: forall f a .
+                           (Vectorize f, Interpolant a, Eq (f Double), Show (f Double))
+                        => String -> (Double -> f Double) -> (a -> Double) -> (Double -> a)
                         -> HUnit.Assertion
-test_knots_interpolant3 solver toDouble fromDouble = HUnit.assert $ do
+test_knots_interpolant3 solver toMulti toDouble fromDouble = HUnit.assert $ do
   let grid0 :: V2 Double
       grid0 = V2 10 15
 
@@ -136,14 +138,15 @@ test_knots_interpolant3 solver toDouble fromDouble = HUnit.assert $ do
       grid2 :: V4 Double
       grid2 = V4 100 200 300 400
 
-      values :: V2 (V3 (V4 Double))
+      values :: V2 (V3 (V4 (f Double)))
       values =
+        (unO . unO . fmap toMulti . O . O) $
         V2
         (V3 (V4 1 2 3 4) (V4 5 6 7 8) (V4 9 10 11 12))
         (V3 (V4 13 14 15 16) (V4 17 18 19 20) (V4 21 22 23 34))
 
-  f <- makeInterpolant3 "test_knots_interpolant3" solver grid0 grid1 grid2 ((unO . unO . fmap Id . O . O) values)
-       :: IO (V3 a -> Id a)
+  f <- makeInterpolant3 "test_knots_interpolant3" solver grid0 grid1 grid2 values
+       :: IO (V3 a -> f a)
 
   let xs :: V2 (V3 (V4 Double))
       xs = fmap (fmap pure) $ fmap pure grid0
@@ -154,8 +157,8 @@ test_knots_interpolant3 solver toDouble fromDouble = HUnit.assert $ do
       zs :: V2 (V3 (V4 Double))
       zs = pure $ pure grid2
 
-      newValues :: V2 (V3 (V4 Double))
-      O (O newValues) = fmap (toDouble . unId . f . fmap fromDouble) (V3 <$> O (O xs) <*> O (O ys) <*> O (O zs))
+      newValues :: V2 (V3 (V4 (f Double)))
+      O (O newValues) = fmap (fmap toDouble . f . fmap fromDouble) (V3 <$> O (O xs) <*> O (O ys) <*> O (O zs))
 
   return $ HUnit.assertEqual "" values newValues
 
@@ -171,11 +174,11 @@ v2345 = r
   where
     O (O (O r)) = realToFrac <$> vcountUp
 
-test_knots_interpolant4 :: forall a .
-                           Interpolant a
-                        => String -> (a -> Double) -> (Double -> a)
+test_knots_interpolant4 :: forall f a .
+                           (Vectorize f, Interpolant a, Eq (f Double), Show (f Double))
+                        => String -> (Double -> f Double) -> (a -> Double) -> (Double -> a)
                         -> HUnit.Assertion
-test_knots_interpolant4 solver toDouble fromDouble = HUnit.assert $ do
+test_knots_interpolant4 solver toMulti toDouble fromDouble = HUnit.assert $ do
   let grid0 :: V2 Double
       grid0 = V2 10 15
 
@@ -188,12 +191,11 @@ test_knots_interpolant4 solver toDouble fromDouble = HUnit.assert $ do
       grid3 :: V5 Double
       grid3 = V5 0.3 0.6 0.9 1.2 1.5
 
-      values :: V2 (V3 (V4 (V5 Double)))
-      values = v2345
+      values :: V2 (V3 (V4 (V5 (f Double))))
+      values = (unO . unO . unO . fmap toMulti . O . O . O) $ v2345
 
-  f <- makeInterpolant4 "test_knots_interpolant4" solver grid0 grid1 grid2 grid3
-       ((unO . unO . unO . fmap Id . O . O . O) values)
-       :: IO (V4 a -> Id a)
+  f <- makeInterpolant4 "test_knots_interpolant4" solver grid0 grid1 grid2 grid3 values
+       :: IO (V4 a -> f a)
 
   let xs :: V2 (V3 (V4 (V5 Double)))
       xs = fmap (fmap (fmap pure)) $ fmap (fmap pure) $ fmap pure grid0
@@ -207,17 +209,30 @@ test_knots_interpolant4 solver toDouble fromDouble = HUnit.assert $ do
       ws :: V2 (V3 (V4 (V5 Double)))
       ws = pure $ pure $ pure grid3
 
-      O (O (O newValues)) = fmap (toDouble . unId . f . fmap fromDouble) (V4 <$> O (O (O xs)) <*> O (O (O ys)) <*> O (O (O zs)) <*> O (O (O ws)))
+      O (O (O newValues)) = fmap (fmap toDouble . f . fmap fromDouble) (V4 <$> O (O (O xs)) <*> O (O (O ys)) <*> O (O (O zs)) <*> O (O (O ws)))
 
   return $ HUnit.assertEqual "" values newValues
 
+data ToMulti where
+  ToMulti :: (Eq (f Double), Show (f Double), Vectorize f)
+          => String -> (Double -> f Double) -> ToMulti
+
+toMultis :: [ToMulti]
+toMultis =
+  [ ToMulti "Id" Id
+  , ToMulti "V2" (\x -> V2 x (100 + x))
+  , ToMulti "V3" (\x -> V3 (2 + x) (x - 1) (22 + x))
+  ]
 
 interpolantTests :: Test
 interpolantTests =
   testGroup "interpolant tests" $
-  [ testGroup "ffi" (toTests id id)
-  , testGroup "DM" (toTests dmToDouble dmFromDouble)
-  , testGroup "MX" (toTests mxToDouble mxFromDouble)
+  [ testGroup "DM" (toTests Id dmToDouble dmFromDouble)
+  , testGroup "MX" (toTests Id mxToDouble mxFromDouble)
+  , testGroup "ffi"
+    [ testGroup name (toTests toMulti id id)
+    | ToMulti name toMulti <- toMultis
+    ]
   ]
   where
     dmToDouble :: S DM -> Double
@@ -235,14 +250,15 @@ interpolantTests =
     mxFromDouble :: Double -> S MX
     mxFromDouble = vcat . Id . UnsafeM . CM.fromDouble
 
-toTests :: Interpolant a => (a -> Double) -> (Double -> a) -> [Test]
-toTests to from =
-  [ testCase "interp1 in between" (test_in_between_interpolant1 solver to from)
-  , testCase "interp2 in between" (test_in_between_interpolant2 solver to from)
-  , testCase "interp1 knots"  (test_knots_interpolant1 solver to from)
-  , testCase "innterp2 knots" (test_knots_interpolant2 solver to from)
-  , testCase "interp3 knots"  (test_knots_interpolant3 solver to from)
-  , testCase "interp4 knots"  (test_knots_interpolant4 solver to from)
+toTests :: (Vectorize f, Interpolant a, Eq (f Double), Show (f Double))
+        => (Double -> f Double) -> (a -> Double) -> (Double -> a) -> [Test]
+toTests toMulti to from =
+  [ testCase "interp1 in between" (test_in_between_interpolant1 solver toMulti to from)
+  , testCase "interp2 in between" (test_in_between_interpolant2 solver toMulti to from)
+  , testCase "interp1 knots" (test_knots_interpolant1 solver toMulti to from)
+  , testCase "interp2 knots" (test_knots_interpolant2 solver toMulti to from)
+  , testCase "interp3 knots" (test_knots_interpolant3 solver toMulti to from)
+  , testCase "interp4 knots" (test_knots_interpolant4 solver toMulti to from)
   ]
   where
     solver = "linear"
